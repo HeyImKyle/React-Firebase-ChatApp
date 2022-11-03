b497a4f2a0c5d4eaaa243209384552279b0d0b1c
WhizBang <Kvance247@gmail.com>
2022-11-03 10:25:28 -0500

    setup components to work on

---
+++ src/components/Chats.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+const Chats = () => {
+  return <div>Chats</div>;
+};
+export default Chats;
---
+++ src/components/Message.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+const Message = () => {
+  return <div>Message</div>;
+};
+export default Message;
