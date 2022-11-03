e8aac558957f3b8c629c907d330c5425ea7d7949
WhizBang <Kvance247@gmail.com>
2022-11-03 12:00:21 -0500

    moving on to firebase

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,5 +1,22 @@
 import React from "react";
 const Message = () => {
-  return <div>Message</div>;
+  return (
+    <div className="message owner">
+      <div className="messageInfo">
+        <img
+          src="https://media.istockphoto.com/photos/mom-and-son-having-fun-outdoor-picture-id1035146258?k=20&m=1035146258&s=612x612&w=0&h=YBcP5MGW_RDEU6oyUEpksPwP4Hks4oS5s_IbKESc7Jg="
+          alt=""
+        />
+        <span>just now</span>
+      </div>
+      <div className="messageContent">
+        <p>Hello</p>
+        <img
+          src="https://media.istockphoto.com/photos/mom-and-son-having-fun-outdoor-picture-id1035146258?k=20&m=1035146258&s=612x612&w=0&h=YBcP5MGW_RDEU6oyUEpksPwP4Hks4oS5s_IbKESc7Jg="
+          alt=""
+        />
+      </div>
+    </div>
+  );
 };
 export default Message;
